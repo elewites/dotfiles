ff2db5d700279979bf429d943f98e694fb1231a5
erosrodr <erosrodv@gmail.com>
2026-08-07 16:35:39 -0700

    install prowlerr and remove avante

--- private_dot_config/private_nvim/private_lua/private_plugins/avante.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/avante.lua
@@ -18,35 +18,35 @@
 -- }
 
 return {
-  "yetone/avante.nvim",
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  -- ⚠️ must add this setting! ! !
-  build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-    or "make",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  ---@module 'avante'
-  ---@type avante.Config
-  opts = {
-    -- add any opts here
-    -- this file can contain specific instructions for your project
-    instructions_file = "avante.md",
-    -- for example
-    provider = "copilot",
-    input = {
-      provider = "snacks",
-    },
-    providers = {
-      -- claude = {
-      --   endpoint = "https://api.anthropic.com",
-      --   model = "claude-sonnet-4-20250514",
-      --   timeout = 30000, -- Timeout in milliseconds
-      --   extra_request_body = {
-      --     temperature = 0.75,
-      --     max_tokens = 20480,
-      --   },
-      -- },
-    },
+  -- "yetone/avante.nvim",
+  -- -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- -- ⚠️ must add this setting! ! !
+  -- build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+  --   or "make",
+  -- event = "VeryLazy",
+  -- version = false, -- Never set this value to "*"! Never!
+  -- ---@module 'avante'
+  -- ---@type avante.Config
+  -- opts = {
+  --   -- add any opts here
+  --   -- this file can contain specific instructions for your project
+  --   instructions_file = "avante.md",
+  --   -- for example
+  --   provider = "copilot",
+  --   input = {
+  --     provider = "snacks",
+  --   },
+  --   providers = {
+  --     -- claude = {
+  --     --   endpoint = "https://api.anthropic.com",
+  --     --   model = "claude-sonnet-4-20250514",
+  --     --   timeout = 30000, -- Timeout in milliseconds
+  --     --   extra_request_body = {
+  --     --     temperature = 0.75,
+  --     --     max_tokens = 20480,
+  --     --   },
+  --     -- },
+  --   },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
